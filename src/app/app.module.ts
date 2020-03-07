67292b08dd50b72f4f80cbc271d92790b99829df
teodora.bene <teo.popa90@gmail.com>
2020-03-07 19:38:02 +0200

    commit changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component'; //and you omit the .ts, you don't add .ts to your import, it's just server.component, so the
+//file name without the extension, the extension is added by webpack which bundles our project automatically.
+import {FormsModule} from '@angular/forms';
+
+import {ServerComponent} from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [ // let us to add other modules
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
